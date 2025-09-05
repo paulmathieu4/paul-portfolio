4ee1974a33ff477a670ef98b9a3ccce6f928243d
paul MATHIEU <paul.mathieu4@gmail.com>
2025-09-05 21:07:19 +0200

    Initial commit, Skeleton install with sveltekit, Lucide icons and an AppBar

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import '../app.css';
+	import favicon from '$lib/assets/favicon.svg';
+	import { AppBar } from '@skeletonlabs/skeleton-svelte';
+	import {ArrowLeft, Calendar, CircleUser, Paperclip} from "lucide-svelte";
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+
+<AppBar>
+	{#snippet lead()}
+		<ArrowLeft size={24} />
+	{/snippet}
+	{#snippet trail()}
+		<Paperclip size={20} />
+		<Calendar size={20} />
+		<CircleUser size={20} />
+	{/snippet}
+	<span>Title</span>
+</AppBar>
+
+{@render children?.()}
